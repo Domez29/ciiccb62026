12791ba5713a2f39b62efded97f00ab97c31a90b
Domenic Garcia <ev.cinemod@gmail.com>
2026-06-06 07:03:34 +0000

    clone

---
+++ Chapter3/task3.java
@@ -0,0 +1,19 @@
+package Chapter3;
+
+public class task3 {
+    public static void main(String[] args) {
+
+        String a = new String("WOW");
+        String b = "WOW";
+        String c = a;
+        String d = c;
+
+        Boolean b1 = a.equals(b);
+        Boolean b2 = d.equals(a);
+        Boolean b3 = c.equals(b);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
