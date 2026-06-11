e70dc5f30f61fd9f0092802d48476faa68e8a195
Domenic Garcia <ev.cinemod@gmail.com>
2026-06-11 08:07:31 +0000

    task6

---
+++ Task/QS.java
@@ -0,0 +1,9 @@
+package Task;
+
+public class QS {
+    String letters = "abcdef";
+System.out.println(letters.length());
+System.out.println(letters.charAt(3));
+System.out.println(letters.charAt(6));
+
+}
