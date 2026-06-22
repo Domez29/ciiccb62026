c72f1168e345fbee1f96d3373d8cf3559b1d9aff
Domenic Garcia <ev.cinemod@gmail.com>
2026-06-22 09:20:26 +0000

    task17

--- Task/Task2/Task16.java
+++ Task/Task2/Task16.java
@@ -1,11 +1,11 @@
 package Task.Task2;
 
 // write a new method in a Point called scale, that will make the closer by the half to (0,0). So for example (8,4) after scale will be (4,2).
-public class Point {
+public class Task16 {
     private int x;
     private int y;
 
-    public Point(int x, int y) {
+    public Task16(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -20,7 +20,7 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        Point p = new Point(8, 4);
+        Task16 p = new Task16(8, 4);
         p.print();  // Output: (8,4)
         
         p.scale();
---
+++ Task/Task2/Task17.java
@@ -0,0 +1,61 @@
+package Task.Task2;
+
+// 1. Create a base class called Vehicle with the fallowing attributes:
+// make (string): the make of vehicle
+// model (string): the model of the vehicle
+// year (int): the manufacturing year of the vehicle
+// 2. Create a derived class called car that extend the Vehicle class. The car class Should have an additional attributes:
+// number of doors (int): The number of doors in the car.
+// 3. Create an instance of the Car class and set values for its attributes.
+// 4. Implement a method in the Car class called display details() that print out the details of the car, including the inherited attributes from the Vehicle class.
+
+public class Task17 {
+    
+    // 1. Create a base class called Vehicle
+class Vehicle {
+    // Attributes for the base class
+    String make;
+    String model;
+    int year;
+
+    // Constructor to initialize Vehicle attributes
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+// 2. Create a derived class called Car that extends the Vehicle class
+class Car extends Vehicle {
+    // Additional attribute specific to Car
+    int numberOfDoors;
+
+    // Constructor initializing both inherited and subclass attributes
+    public Car(String make, String model, int year, int numberOfDoors) {
+        // 'super' passes the inherited attributes to the Vehicle constructor
+        super(make, model, year); 
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    // 4. Implement a method in the Car class called displayDetails()
+    public void displayDetails() {
+        System.out.println("--- Car Details ---");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+}
+
+// Main class to run and test the implementation
+public class Main {
+    public static void main(String[] args) {
+        // 3. Create an instance of the Car class and set values for its attributes
+        Car myCar = new Car("Mitsubishi", "Expander", 2026, 5);
+
+        // Call the method to display details
+        myCar.displayDetails();
+    }
+}
+}
